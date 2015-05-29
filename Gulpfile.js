19aa2f6d5939c1c17913869f4a3e5bfed1217f59
Martin Woitzik <m.woitzik@gmx.de>
2015-05-29 11:15:42 +0200

    Add another foomodule to test dependencies and import it in foomodule1

--- Gulpfile.js
+++ Gulpfile.js
@@ -109,7 +109,13 @@ gulp.task('templates', function () {
 
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch(['app/**/*.js', 'app/**/*.html'], [
+  gulp.watch([
+    'app/**/*.js',
+    'app/**/*.html',
+    'app/**/*.scss',
+    '!app/_dependencies.js',
+    '!app/_modules.scss'
+  ], [
     'build'
   ], function(err) {
     console.log(err);
--- app/src/ui/shared/foomodule1/index.js
+++ app/src/ui/shared/foomodule1/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 require('angular').module('foomodule1', [
-  'portfolio.templates'
+  'portfolio.templates',
+  'foomodule3'
   ])
   .controller('FooModule1Controller', require('./foomodule1.controller'))
   .directive('foomodule1', require('./foomodule1.directive'));
